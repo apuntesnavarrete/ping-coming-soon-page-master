459de8b8ca50c9e410037287270d8e61284be112
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-05-31 18:00:45 -0500

    movil css y js funcionando

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 let ingreso = document.getElementById("correo")
 let parrafo_error = document.getElementsByClassName("form_error")[0]
+let input = document.getElementsByClassName("main_input")[0]
+
+
 
 const expresiones = {
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
@@ -10,8 +13,12 @@ ingreso.addEventListener("keyup",(e)=>{
     console.log(texto)
     if (expresiones.correo.test(texto)){
         parrafo_error.style.display="none";
+        input.style.outline="0.5px solid var(--Pale-Blue)";
+
         } else{
         parrafo_error.style.display="block";
+        input.style.outline="0.5px solid var(--Light-Red)";
+
     }
     
 }
